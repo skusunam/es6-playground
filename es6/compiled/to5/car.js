634b29a6566d583d08482a69c4759452392efa5c
Srini Kusunam <skusunam@gmail.com>
2014-12-21 20:19:29 -0600

    Added project structure and also gulp with traceur and 6to5 transpilers with watch task

---
+++ es6/compiled/to5/car.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * Created by kusunams on 12/21/14.
+ */
+
+var car = function car(engine) {
+  this.engine = engine;
+};
---
+++ es6/compiled/traceur/car.js
@@ -0,0 +1,5 @@
+"use strict";
+var car = function car(engine) {
+  this.engine = engine;
+};
+($traceurRuntime.createClass)(car, {}, {});
